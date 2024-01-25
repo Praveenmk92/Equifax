ef212b6083dffd709271b0cab9128c80b6afb587
Praveenmk92 <157297612+Praveenmk92@users.noreply.github.com>
2024-01-25 08:27:25 +0530

    Lables_EC2.tf

--- Lables_EC2.tf
+++ Lables_EC2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 # Using the count variable to create multiple instances
-resource "aws_instance" "Myinstances" {
+resource "aws_instance" "Mynewinstances" {
   count         = 2
   ami           = "ami-0a3c3a20c09d6f377"
   instance_type = "t2.micro"
